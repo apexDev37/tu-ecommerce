c011cdd0017472b2830b86ee41883a48f8c432e2
apexdev <mwangi.em37@gmail.com>
2022-07-19 00:11:39 +0300

    Updated Customer Models
    Included customer model.

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50) 
+    last_name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+    comments = models.CharField()
+
+    def __str__(self) -> str:
+        return (f'<Id: {self.id} Name: {self.first_name} Contact: {self.contact}')
+
+
+
