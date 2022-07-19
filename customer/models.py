c5489df0f6adb61e97e8519348255d72ee6cd6c7
apexdev <mwangi.em37@gmail.com>
2022-07-19 15:24:00 +0300

    Updated Models in Customer App
    Included Destination and Address models.

--- customer/models.py
+++ customer/models.py
@@ -1,6 +1,7 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+# Models for the Customer app.
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50) 
@@ -13,4 +14,22 @@ class Customer(models.Model):
         return (f'<Id: {self.id} Name: {self.first_name} Contact: {self.contact}')
 
 
+class Destination(models.Model):
+    state = models.CharField(max_length=20)
+    county = models.CharField(max_length=20)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return (f'<Id: {self.id} State: {self.state} Contact: {self.county}')
+
+
+class Address(models.Model):
+    building_name = models.CharField(max_length=50)
+    building_no = models.Integer()
+    description = models.CharField(blank=True)
+    floor = models.Integer()
+    street_name = models.CharField(max_length=50)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return (f'<Id: {self.id} Building Name: {self.building_name}')
