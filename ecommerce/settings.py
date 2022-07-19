c6191a326f74142851452e65d526507fd5a3525b
apexdev <mwangi.em37@gmail.com>
2022-07-19 16:40:19 +0300

    Initial Product App Migration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'customer.apps.CustomerConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
+from unicodedata import category, name
 from django.db import models
 
-# Create your models here.
+# Models for the Product App.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+    image_path = models.TextField()
