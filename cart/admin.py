bbb104a4b02340e0d9744f670b8060b12bd74b0a
apexdev <mwangi.em37@gmail.com>
2022-07-19 21:35:48 +0300

    Admin Apps Registration
    Registered all app models into the admin site.

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,12 @@
+from ast import Or
 from django.contrib import admin
+from .models import (
+    Cart,
+    Order
+)
+
 
 # Register your models here.
+
+admin.site.register(Cart)
+admin.site.register(Order)
--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Customer,
+    Destination,
+    Address
+)
+
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Destination)
+admin.site.register(Address)
