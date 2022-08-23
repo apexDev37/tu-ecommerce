ecb55984d33df98913262b37a4e745d813e5b36e
apexdev <mwangi.em37@gmail.com>
2022-08-23 16:55:49 +0300

    style: Format customer module
    Format Python code with autopep8 to conform to the PEP 8 style guide.

--- customer/models.py
+++ customer/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Models for the Customer app.
 
+
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50) 
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     contact = models.CharField(max_length=10)
     email = models.CharField(max_length=50)
     comments = models.TextField(blank=True, null=True)
 
     def __str__(self) -> str:
-        return (f'<Id: {self.id} Name: {self.first_name} Contact: {self.contact}')
+        return (
+            f'<Id: {self.id} Name: {self.first_name} Contact: {self.contact}')
 
 
 class Destination(models.Model):
